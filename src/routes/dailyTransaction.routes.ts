9b3e3c3edefb9e343a072d6fdb376dd2b69e510d
Carlos Moreira <cesmoreira90@hotmail.com>
2025-06-21 19:20:11 +0100

    fix: error 401 when client login

--- src/routes/dailyTransaction.routes.ts
+++ src/routes/dailyTransaction.routes.ts
@@ -1,31 +1,26 @@
 import { Router } from "express";
 import { DailyTransactionController } from "../controllers/DailyTransactionController";
-import {authMiddleware, roleMiddleware} from "../middlewares";
-import {RoleType} from "../entities/Role";
+import { authMiddleware } from "../middlewares";
 
 const router = Router();
 const dailyTransactionController = new DailyTransactionController();
 
-router.use(authMiddleware);
-router.get("/", dailyTransactionController.getAll);
-router.get("/client/:clientId", [
-    roleMiddleware([RoleType.CLIENT, RoleType.ADMIN]),
-    ...dailyTransactionController.clientIdValidation
-], dailyTransactionController.getByClientId);
-router.get("/client/:clientId/date/:date", [
+router.get("/",authMiddleware, dailyTransactionController.getAll);
+router.get("/client/:clientId",[authMiddleware, ...dailyTransactionController.clientIdValidation], dailyTransactionController.getByClientId);
+router.get("/client/:clientId/date/:date", [authMiddleware,
     ...dailyTransactionController.clientIdValidation,
     ...dailyTransactionController.dateValidation
 ], dailyTransactionController.getClientTransactionsByDate);
-router.get("/date/:date", [...dailyTransactionController.dateValidation], dailyTransactionController.getByDate);
-router.get("/year/:year/month/:month", [...dailyTransactionController.yearMonthValidation], dailyTransactionController.getByMonth);
-router.get("/:id", [...dailyTransactionController.idValidation], dailyTransactionController.getById);
-router.post("/", [...dailyTransactionController.transactionValidation], dailyTransactionController.create);
-router.put("/:id", [
+router.get("/date/:date", [authMiddleware,...dailyTransactionController.dateValidation], dailyTransactionController.getByDate);
+router.get("/year/:year/month/:month", [authMiddleware,...dailyTransactionController.yearMonthValidation], dailyTransactionController.getByMonth);
+router.get("/:id", [authMiddleware,...dailyTransactionController.idValidation], dailyTransactionController.getById);
+router.post("/", [authMiddleware,...dailyTransactionController.transactionValidation], dailyTransactionController.create);
+router.put("/:id", [authMiddleware,
     ...dailyTransactionController.idValidation,
     ...dailyTransactionController.transactionValidation
 ], dailyTransactionController.update);
-router.delete("/:id", [...dailyTransactionController.idValidation], dailyTransactionController.delete);
-router.get("/sum/date/:date", [...dailyTransactionController.dateValidation], dailyTransactionController.getSumByDate);
-router.get("/sum/year/:year/month/:month", [...dailyTransactionController.yearMonthValidation], dailyTransactionController.getSumByMonth);
+router.delete("/:id", [authMiddleware,...dailyTransactionController.idValidation], dailyTransactionController.delete);
+router.get("/sum/date/:date", [authMiddleware,...dailyTransactionController.dateValidation], dailyTransactionController.getSumByDate);
+router.get("/sum/year/:year/month/:month", [authMiddleware,...dailyTransactionController.yearMonthValidation], dailyTransactionController.getSumByMonth);
 
 export default router;
